35929614c05fab9f925f848e40393e5f61e85cdc
Jiawen He <hanghur@gmail.com>
2023-11-29 09:04:49 +1100

    feat: add bayesbridge example

---
+++ examples/surface_wave_trans_d/surface_wave_bayesbridge.py
@@ -0,0 +1,159 @@
+from typing import Union
+from numbers import Number
+import numpy as np
+import random
+from pysurf96 import surf96
+import cofi
+import bayesbridge as bb
+
+
+# -------------- Setting up constants, fwd func, synth data
+VP_VS = 1.77
+RAYLEIGH_STD = 0.02
+LOVE_STD = 0.02
+RF_STD = 0.03
+LAYERS_MIN = 3
+LAYERS_MAX = 15
+LAYERS_INIT_RANGE = 0.3
+VS_PERTURB_STD = 0.15
+VS_UNIFORM_MIN = [2.7, 3.2, 3.75]
+VS_UNIFORM_MAX = [4, 4.75, 5]
+VS_UNIFORM_POS = [0, 40, 80]
+VORONOI_PERTURB_STD = 8
+VORONOI_POS_MIN = 0
+VORONOI_POS_MAX = 130
+N_CHAINS = 2
+
+
+def _calc_thickness(sites: np.ndarray):
+    depths = (sites[:-1] + sites[1:]) / 2
+    thickness = np.hstack((depths[0], depths[1:] - depths[:-1], 0))
+    return thickness
+
+def _get_thickness(model: bb.State):
+    sites = model.voronoi_sites
+    if model.has_cache("thickness"):
+        thickness = model.load_cache("thickness")
+    else:
+        thickness = _calc_thickness(sites)
+        model.store_cache("thickness", thickness)
+    return thickness
+
+def forward_sw(model, periods, wave="rayleigh", mode=1):
+    vs = model.get_param_values("vs")
+    thickness = _get_thickness(model)
+    vp = vs * VP_VS
+    rho = 0.32 * vp + 0.77
+    return surf96(
+        thickness,
+        vp,
+        vs,
+        rho,
+        periods,
+        wave=wave,
+        mode=mode,
+        velocity="phase",
+        flat_earth=False,
+    )
+
+def forward_sw_rayleigh(model):
+    return forward_sw(model, periods1, "rayleigh", 1)
+
+def forward_sw_love(model):
+    return forward_sw(model, periods1, "love", 1)
+
+true_thickness = np.array([10, 10, 15, 20, 20, 20, 20, 20, 0])
+true_voronoi_positions = np.array([5, 15, 25, 45, 65, 85, 105, 125, 145])
+true_vs = np.array([3.38, 3.44, 3.66, 4.25, 4.35, 4.32, 4.315, 4.38, 4.5])
+true_model = bb.State(len(true_vs), true_voronoi_positions, {"vs": true_vs})
+
+periods1 = np.linspace(4, 80, 20)
+rayleigh1 = forward_sw_rayleigh(true_model)
+rayleigh1_dobs = rayleigh1 + np.random.normal(0, RAYLEIGH_STD, rayleigh1.size)
+love1 = forward_sw_love(true_model)
+love1_dobs = love1 + np.random.normal(0, LOVE_STD, love1.size)
+
+
+# -------------- Define bayesbridge objects
+targets = [
+    bb.Target("rayleigh1", rayleigh1_dobs, covariance_mat_inv=1 / RAYLEIGH_STD**2),
+    bb.Target("love1", love1_dobs, covariance_mat_inv=1 / LOVE_STD**2),
+]
+
+fwd_functions = [forward_sw_rayleigh, forward_sw_love]
+
+param_vs = bb.parameters.UniformParameter(
+    name="vs",
+    vmin=VS_UNIFORM_MIN,
+    vmax=VS_UNIFORM_MAX,
+    perturb_std=VS_PERTURB_STD,
+    position=VS_UNIFORM_POS, 
+)
+
+
+def param_vs_initialize(
+    param: bb.parameters.Parameter, 
+    positions: Union[np.ndarray, Number]
+) -> Union[np.ndarray, Number]: 
+    vmin, vmax = param.get_vmin_vmax(positions)
+    if isinstance(positions, (float, int)):
+        return random.uniform(vmin, vmax)
+    sorted_vals = np.sort(np.random.uniform(vmin, vmax, positions.size))
+    for i in range (len(sorted_vals)):
+        val = sorted_vals[i]
+        vmin_i = vmin if np.isscalar(vmin) else vmin[i]
+        vmax_i = vmax if np.isscalar(vmax) else vmax[i]
+        if val < vmin_i or val > vmax_i:
+            if val > vmax_i: sorted_vals[i] = vmax_i
+            if val < vmin_i: sorted_vals[i] = vmin_i
+    return sorted_vals
+
+
+param_vs.set_custom_initialize(param_vs_initialize)
+free_parameters = [param_vs]
+
+parameterization = bb.Voronoi1D(
+    voronoi_site_bounds=(VORONOI_POS_MIN, VORONOI_POS_MAX),
+    voronoi_site_perturb_std=VORONOI_PERTURB_STD,
+    n_voronoi_cells=None,
+    n_voronoi_cells_min=LAYERS_MIN,
+    n_voronoi_cells_max=LAYERS_MAX,
+    free_params=free_parameters,
+    voronoi_cells_init_range=LAYERS_INIT_RANGE, 
+    birth_from="prior",  # or "neighbour"
+)
+
+# -------------- Implement distribution functions
+my_log_prior_ratio_funcs = parameterization.log_prior_ratio_functions
+my_log_likelihood_ratio = bb.LogLikelihood(targets, fwd_functions)
+
+
+# -------------- Implement perturbation functions
+my_perturbation_funcs = parameterization.perturbation_functions
+
+
+# -------------- Initialize walkers
+walkers_start = []
+for i in range(N_CHAINS):
+    walkers_start.append(parameterization.initialize())
+
+
+# -------------- Define CoFI problem
+sw_problem = cofi.BaseProblem()
+
+
+# -------------- Define CoFI inversion options
+inv_options = cofi.InversionOptions()
+inv_options.set_tool("bayesbridge")
+inv_options.set_params(
+    log_prior_ratio_funcs = my_log_prior_ratio_funcs, 
+    log_like_ratio_func = my_log_likelihood_ratio, 
+    perturbation_funcs = my_perturbation_funcs, 
+    walkers_starting_models = walkers_start, 
+    n_chains = N_CHAINS, 
+)
+
+
+# -------------- Run CoFI inversion 
+sw_inversion = cofi.Inversion(sw_problem, inv_options)
+sw_inversion.run()
